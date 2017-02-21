d9faa377190277560dd8d3b5d3b6dcd996973b87
Kla3mus <bjorniboy@gmail.com>
2017-02-21 08:59:46 +0100

    Update select2.ts

--- select2.ts
+++ select2.ts
@@ -4,37 +4,82 @@ import 'select2';
 
 @customElement('select2') 
 @autoinject
-export class Select2 {
+export class Select2Thing {
     @bindable({ defaultBindingMode: bindingMode.twoWay }) name:string = 'select';
     @bindable({ defaultBindingMode: bindingMode.twoWay }) labelproperty: string = 'label';
     @bindable({ defaultBindingMode: bindingMode.twoWay }) valueproperty: string = 'value';
     @bindable({ defaultBindingMode: bindingMode.twoWay }) options: any[] = []; 
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) multiselect: boolean = false; 
     @bindable({ defaultBindingMode: bindingMode.twoWay, changeHandler: 'selectedIsChanged' }) selected: any = null; 
 
     select2 = null;
     constructor(private element: Element) {}
 
-
     selectedIsChanged() {
         if (this.select2 != null) {
-            this.select2.val(this.selected).trigger("change");
+            
+            if (this.shouldISetSelected()) {
+                if (this.multiselect) {
+                    if (this.selected.constructor === Array)
+                        this.select2.val(this.selected).trigger("change");
+                    else
+                        this.select2.val([this.selected]).trigger("change");
+                }
+                else {
+                    this.select2.val(parseInt(this.selected)).trigger("change");
+                }
+            }
+        }
+    }
+    private getMultiValues() {
+        let select = [];
+        if (this.select2.val() == null)
+            return select;
+
+        for (let e of this.select2.val()) {
+            select.push(parseInt(e));
+        }
+        return select;
+    }
+
+    private getSingleValue() {
+        return parseInt(this.select2.val());
+    }
+
+    private shouldISetSelected() {
+        if (this.multiselect) {
+            let mh = this.getMultiValues();
+            if (JSON.stringify(this.selected.sort()) === JSON.stringify(mh.sort())) {
+                return false;
+            }
+            return true;
+        }
+        else {
+            if (this.selected != this.select2.val() && this.select2.val() !== undefined) {
+                return true;
+            }
+            return false;
         }
     }
 
     attached() {
-
         this.select2 = $(this.element).find('select').select2(); //Create the select2 dropdown
         this.select2.val(this.selected).trigger("change"); //Select the one that is set to default select.
-
         this.select2.on('change', evt => {
-
-            if (this.selected != $(evt.delegateTarget.selectedOptions).val() && $(evt.delegateTarget.selectedOptions).val() !== undefined)
-                this.selected = $(evt.delegateTarget.selectedOptions).val();
-
+            
+            if (this.shouldISetSelected()) {
+                if (this.multiselect)
+                    this.selected = this.getMultiValues();
+                else
+                    this.selected = this.getSingleValue();
+            }
+            
+            /*
             if (evt.originalEvent) {
                 return;
             }
             this.element.dispatchEvent(new Event('change'));
+            */
         });
     };
  
